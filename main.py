89711d46cea1d34f57116668f4166d89f3d15ea0
vovka <@vovka>
2025-02-24 18:04:45 +0300

    create __init__ for class Product and Category, edit type products in class Category add count_category and count_products for Category

--- main.py
+++ main.py
@@ -4,9 +4,34 @@ class Product:
     price: int
     quantity: int
 
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
 
 class Category:
     name: str
     description: str
-    products: str
+    products: [Product]
+    count_category = 0
+    count_products = 0
 
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.count_category += 1
+        Category.count_products += len(products)
+
+
+if __name__ == "__main__":
+    pr_1 = Product("g", "g", 3, 4)
+    pr_2 = Product("h", "h", 5, 6)
+
+    ct_1 = Category("t", "t", [pr_1, pr_2])
+    ct_2 = Category("t", "t", [pr_2])
+
+    print(ct_1.name)
+    print(ct_1.count_products)
