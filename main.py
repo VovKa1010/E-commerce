d68f8fdd5778c92e0db53204509cf22647f88d9a
vovka <@vovka>
2025-02-27 17:11:44 +0300

    edit private price in class Product add getter and setter for price

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            if new_price < self.__price:
+                confirmation = input("Цена снижается. Подтвердите действие.(y)\n")
+                if confirmation != "y":
+                    return
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
 
     @classmethod
     def new_product(cls, new_product: dict, old_products: list = None):
