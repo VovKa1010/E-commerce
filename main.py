4cfae9a2837728502fd519c0af8d6b457767886c
vovka <@vovka>
2025-02-27 16:36:23 +0300

    edit Category.products private add method add_product

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ class Product:
 class Category:
     name: str
     description: str
-    products: [Product]
+    __products: [Product]
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
