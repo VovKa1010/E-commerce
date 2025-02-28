c53d3a9d177d685d3dc4057909103ec33aa4af16
vovka <@vovka>
2025-02-28 18:33:18 +0300

    add __add__ for class Product formula: (pr_1.price * pr_1.quantity) + (pr_2.price * pr_2.quantity)

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
+    def __add__(self, other):
+        if isinstance(other, Product):
+            return (self.price * self.quantity) + (other.price * other.quantity)
+
     @property
     def price(self):
         return self.__price
